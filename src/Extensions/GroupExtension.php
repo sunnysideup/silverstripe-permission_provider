8fb391a90e3e9ebfb3ed0239251c601fcb9bbfd2
Nicolaas <github@sunnysideup.co.nz>
2023-08-21 12:29:47 +1200

    MINOR: add ability to choose redirection

--- src/Extensions/GroupExtension.php
+++ src/Extensions/GroupExtension.php
@@ -4,6 +4,7 @@ namespace Sunnysideup\PermissionProvider\Extensions;
 
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\ReadonlyField;
+use SilverStripe\Forms\TextField;
 use SilverStripe\ORM\DataExtension;
 use Sunnysideup\PermissionProvider\Tasks\PermissionProviderBuildTask;
 
@@ -17,6 +18,7 @@ class GroupExtension extends DataExtension
 {
     private static $db = [
         'MainPermissionCode' => 'Varchar',
+        'DefaultLoginLink' => 'Text',
     ];
 
     public function updateCMSFields(FieldList $fields)
@@ -35,8 +37,13 @@ class GroupExtension extends DataExtension
                 ReadonlyField::create('MainPermissionCode', 'Main Permission Code'),
             ]
         );
-
-        return $fields;
+        $fields->addFieldsToTab(
+            'Root.Redirect',
+            [
+                TextField::create('DefaultLoginLink', 'Default Login Link')
+                    ->setDescription('Optional. This is the link that the user, who belongs to this group, will be redirected to after login. If they belong to more than one group with redirection then you can not sure how they will be redirected.'),
+            ]
+        );
     }
 
     public function requireDefaultRecords()
---
+++ src/Extensions/LoginHandlerExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Sunnysideup\PermissionProvider\Extensions;
+
+use SilverStripe\Control\Controller;
+use SilverStripe\Core\Config\Config;
+use SilverStripe\Core\Extension;
+use SilverStripe\Security\Member;
+use SilverStripe\Security\MemberAuthenticator\LoginHandler;
+use SilverStripe\Security\Security;
+
+/**
+ * Class \Sunnysideup\PermissionProvider\Extensions\LoginHandlerExtension.
+ *
+ * @property LoginHandler|LoginHandlerExtension $owner
+ */
+class LoginHandlerExtension extends Extension
+{
+    public function afterLogin(?Member $member = null)
+    {
+        if ($member) {
+            $redirectorGroup = $member->Groups()->filter('DefaultLoginLink:not', ['', null])->first();
+            if ($redirectorGroup) {
+                Config::modify()->merge(
+                    Security::class,
+                    'default_login_dest',
+                    $redirectorGroup->DefaultLoginLink
+                );
+            }
+        }
+    }
+
+}
