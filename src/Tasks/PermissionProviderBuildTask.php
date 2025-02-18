89e416acbe9074b97ad8890617e6ab772fa52513
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2025-02-19 12:01:35 +1300

    ENH: check for non-existing permissions

--- src/Tasks/PermissionProviderBuildTask.php
+++ src/Tasks/PermissionProviderBuildTask.php
@@ -7,6 +7,7 @@ use SilverStripe\Core\ClassInfo;
 use SilverStripe\Dev\BuildTask;
 use SilverStripe\ORM\DB;
 use SilverStripe\Security\Permission;
+use SilverStripe\Security\PermissionRoleCode;
 use Sunnysideup\PermissionProvider\Api\PermissionProviderFactory;
 use Sunnysideup\PermissionProvider\Interfaces\PermissionProviderFactoryProvider;
 
@@ -31,6 +32,8 @@ class PermissionProviderBuildTask extends BuildTask
         $this->deletePermissionsNoLongerRequired();
         $this->createDefaultPermissions();
         $this->deletePermissionsNoLongerRequired();
+        $this->removeObsoletePermissionCodeRoles();
+        echo 'done';
     }
 
     protected function createDefaultPermissions()
@@ -47,6 +50,11 @@ class PermissionProviderBuildTask extends BuildTask
         DB::query('DELETE FROM PermissionRoleCode WHERE Code IS NULL');
     }
 
+    /**
+     *
+     * @todo: fix
+     * @return void
+     */
     protected function deletePermissionsNoLongerRequired()
     {
         if ($this->_permissions !== []) {
@@ -55,6 +63,20 @@ class PermissionProviderBuildTask extends BuildTask
                 if (isset($this->_permissions[$permission->Code]) && 0 === $permission->Arg && 1 === $permission->Type) {
                     DB::alteration_message('Deleting double permission with code: ' . $permission->Code, 'deleted');
                     $permission->delete();
+                }
+            }
+        }
+    }
+
+    protected function removeObsoletePermissionCodeRoles()
+    {
+        $codes = Permission::get_codes(false);
+        $list = PermissionRoleCode::get();
+        foreach ($list as $code) {
+            foreach ($list as $code) {
+                if (! isset($codes[$code->Code])) {
+                    DB::alteration_message('Deleting obsolete permission code role with code: ' . $code->Code, 'deleted');
+                    $code->delete();
                 }
             }
         }
